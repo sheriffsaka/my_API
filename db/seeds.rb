a66058b93b6ea7f3f509502cdff95fd41f60821e
Saka Sheriff Alade <sheriffdeenalade@gmail.com>
2023-08-19 03:17:16 +0100

    modified the production setting

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 # Animal.validates :aname, uniqueness: true, on: :create
-1500.times do
-    Animal.create({
-        aname: Faker::Creature:: Animal.name
-    })
-end
+# 1500.times do
+#     Animal.create({
+#         aname: Faker::Creature:: Animal.name
+#     })
+# end
 
 # Animal.clear_validators!
+
+require 'csv'
+
+CSV.foreach('public\animals.csv', headers: true) do |row|
+  Animal.create!(id: row['id'], aname: row['aname'], created_at: row['created_at'], updated_at: row['updated_at'])
+end
